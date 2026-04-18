094f2cd74fbc790b1d247cd20b5d7b8486e8c330
AllenDinesh <dineshtestingengg@gmail.com>
2026-04-19 00:53:53 +0530

    Add files via upload

---
+++ Day 2 Assignment.js
@@ -0,0 +1,32 @@
+let firstname = "AllenDinesh"
+firstname = "Dinesh"
+console.log('firstname:', firstname, 'and', 'Datatype:', typeof firstname );
+
+let companyname = "Testleaf"
+companyname = "Exela"
+console.log('companyname:', companyname, 'and', 'Datatype:', typeof companyname );
+
+let mobilenumber = 123456789
+mobilenumber = 987654321
+console.log("mobilenumber:", mobilenumber, 'and', 'Datatype:', typeof mobilenumber );
+
+let number = 1234567890123456789n
+number = 9876543210987654321n
+console.log('number:', number, 'and', 'Datatype:', typeof number );
+
+let isAutomation = false
+isAutomation = true
+console.log('isAutomation:', !isAutomation, 'and', 'Datatype:', typeof isAutomation );
+
+let hasplaywright 
+hasplaywright = null
+console.log('hasplaywright:', hasplaywright, 'and', 'Datatype:', typeof hasplaywright );
+
+let Typescript 
+console.log('Typescript:', Typescript, 'and', 'Datatype:', typeof Typescript);
+
+
+
+
+
+
